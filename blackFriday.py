1fe0a874e4ea357cbd70700629dbbacee5696f59
Cindyakaxinyi <xinyi.li@rbe.sk.ca>
2020-02-24 08:42:04 -0600

    commity

---
+++ blackFriday.py
@@ -0,0 +1,18 @@
+# Cindy Li
+# Feb 24, 2020
+# Black Friday shopping simulator at Target's appliances section
+
+characters = ["Target employee", "Karen", "security", "crowd"]
+# game characters
+items = ["Samsung smart TV", "LG fridge", "Marble top microwave",
+        "Gold plated toddler walker", "Rice cooker"]
+# appliances at Target
+
+print("Characters in the game:")
+for character in characters:
+    print(character)
+
+print(" ")
+print("Appliances at target:")
+for item in items:
+    print (item)
--- rpg-inventory.py
+++
@@ -1,7 +0,0 @@
-# Cindy Li
-# Feb 24, 2020
-# Black Friday shopping simulator
-
-characters = ["Target employee", "Karen", "security", "crowd"]
-# game characters
-items = ["Samsung smart TV", "LG fridge", ]
