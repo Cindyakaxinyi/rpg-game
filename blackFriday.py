b7a5516bad5dd77032d9244c7c2533a5f5418395
Cindyakaxinyi <xinyi.li@rbe.sk.ca>
2020-02-24 09:07:36 -0600

    update blackFriday.py

--- blackFriday.py
+++ blackFriday.py
@@ -2,17 +2,38 @@
 # Feb 24, 2020
 # Black Friday shopping simulator at Target's appliances section
 
-characters = ["Target employee", "Karen", "security", "crowd"]
 # game characters
-items = ["Samsung smart TV", "LG fridge", "Marble top microwave",
-        "Gold plated toddler walker", "Rice cooker"]
+characters = ["Target employee", "Karen", "security", "crowd"]
 # appliances at Target
-
+items = ["Samsung smart TV", "LG fridge", "Marble top microwave",
+         "Gold plated toddler walker", "Rice cooker"]
+# prints characters
 print("Characters in the game:")
 for character in characters:
     print(character)
-
+# prints appliances
 print(" ")
 print("Appliances at target:")
 for item in items:
     print (item)
+
+# sorted() command
+print(" ")
+print("Appliances in alphabetical order:")
+print(sorted(items))
+
+# len() command
+print(" ")
+print("There are " + str(len(characters)) + " characters in this game")
+
+# reverse() command
+print(" ")
+print("Characters in reversed order")
+characters.reverse()
+print(characters)
+
+# sort() command
+print(" ")
+print("Characters in alphabetical order:")
+characters.sort()
+print(characters)
