d2b73afcfc137800ef16c8166ea7e0f751c6a994
Cindyakaxinyi <xinyi.li@rbe.sk.ca>
2020-03-10 08:54:02 -0600

    Update nested-d.py

---
+++ nested-d.py
@@ -0,0 +1,6 @@
+# dictionaries for appliances and their prices
+
+prices = {
+          "Samsung smart TV": 1 099.99, "LG minifridge": 138.50,
+          "Marble top microwave": 115.10, "Rice cooker": 149.69,
+          "Gold plated toddler walker": 3 420.14}
