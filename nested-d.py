1eec0ff568d30c553e8eabdf7d20a28e32370111
Cindyakaxinyi <xinyi.li@rbe.sk.ca>
2020-03-10 09:00:57 -0600

    Update nested-d.py

--- nested-d.py
+++ nested-d.py
@@ -1,6 +1,11 @@
-# dictionaries for appliances and their prices
+# dictionary for appliances and their prices
+# dictionary for characters and their weapons
 
-prices = {
-          "Samsung smart TV": 1 099.99, "LG minifridge": 138.50,
+prices = {"Samsung smart TV": 1 099.99, "LG minifridge": 138.50,
           "Marble top microwave": 115.10, "Rice cooker": 149.69,
-          "Gold plated toddler walker": 3 420.14}
+          "Gold plated toddler walker": 3 420.14
+          }
+
+weapons = {"Security guard": {"tazer", "police stick", "pepper spray"},
+            "Karen": "AIYUQI Loafers Genuine Leather Flat ladies shoes 2019",
+            "Cashier": "portable cash register"}
